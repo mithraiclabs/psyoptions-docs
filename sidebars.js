40a096b14fd9bf5114f59b2c55153ea5e48f9432
Taylor123 <cjohns28@terpmail.umd.edu>
2021-04-25 16:56:50 +0800

    improve beta testing page and add option type architecture  thoughts

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     "Getting Started": ["intro", "glossary"],
+    "Architecture": ["arch-option-types"],
     "Program Integration": ["program-integration-start"],
     Clients: [
       "client-integration-overview",
